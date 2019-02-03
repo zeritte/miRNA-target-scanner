bf91eaeb8d74b39d568694b8da8740fb2a65d119
zeritte <zeritte@gmail.com>
2019-02-03 15:13:59 +0300

    hello.html added

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    form = ReusableForm(request.form)
+    print(form.errors)
+    if request.method == 'POST':
+        name=request.form['name']
+        print(name)
+
+    if form.validate():
+        # Save the comment here.
+        return redirect("/results/%s" %name)
+    else:
+        flash('enter a miRNA name')
+    return render_template('hello.html', form=form)
 
 @app.route("/results/<name>")
 def results(name):
