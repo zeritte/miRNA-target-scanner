d6cb73bd8670d02b963c555393b5c2dafb7943b6
zeritte <zeritte@gmail.com>
2019-02-03 15:25:38 +0300

    a few fixes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, request
+from wtforms import Form, validators, TextField
 app = Flask(__name__)
+
+app.secret_key = "super secret key"
 
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
 
-@app.route('/')
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
     form = ReusableForm(request.form)
     print(form.errors)
